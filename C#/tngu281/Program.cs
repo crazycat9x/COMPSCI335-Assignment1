f4ce82be5e505951cd3ec1ea2eb06c9954151a40
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-07-17 10:21:29 +1200

    node ver 3

---
+++ C#/tngu281/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tngu281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- node js/index.js
+++ node js/index.js
@@ -4,10 +4,11 @@ const [fileName, k] = process.argv.slice(-2);
 const data = fs
   .readFileSync(fileName, "utf8")
   .toUpperCase()
-  .split(/[^A-Z]+/)
-  .filter(word => word);
+  .match(/[A-Z]+/g);
 
 [...new Set(data)]
   .map(element => [element, data.filter(word => word === element).length])
   .sort((a, b) => b[1] - a[1])
-  .forEach((element, index) => index < k && console.log(`${element[1]} ${element[0]}`));
+  .forEach(
+    (element, index) => index < k && console.log(`${element[1]} ${element[0]}`)
+  );
