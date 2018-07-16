a53d0f924bf858ca9b44f34f12bcf4403c4da0b6
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-07-17 01:58:59 +1200

    node js second ver

--- node js/index.js
+++ node js/index.js
@@ -1,22 +1,13 @@
 const fs = require("fs");
-
 const [fileName, k] = process.argv.slice(-2);
 
 const data = fs
   .readFileSync(fileName, "utf8")
-  .replace(/[\W\s0-9]+/gi, " ")
   .toUpperCase()
-  .trim()
-  .split(" ");
+  .split(/[^A-Z]+/)
+  .filter(word => word);
 
 [...new Set(data)]
-  .map(element => [
-    element,
-    data.reduce((accumulator, currentValue) => {
-      return currentValue === element ? ++accumulator : accumulator;
-    }, 0)
-  ])
+  .map(element => [element, data.filter(word => word === element).length])
   .sort((a, b) => b[1] - a[1])
-  .forEach(
-    (element, index) => index < k && console.log(`${element[1]} ${element[0]}`)
-  );
+  .forEach((element, index) => index < k && console.log(`${element[1]} ${element[0]}`));
